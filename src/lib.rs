40b6323ea5b2c3d5a67ba0ff6bb505dc61d209e7
sagan <camjanssen120@gmail.com>
2024-05-06 01:20:05 -0700

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,6 @@
 mod framedata;
 pub mod character;
 
-pub(crate) mod selectors {}
-
-#[derive(Debug, Clone)]
-pub struct Move {
-    pub identifier: String,
-    pub input: String,
-    pub name: String,
-    pub image_link: String,
-    pub damage: String,
-    pub chip_damage: String,
-    pub damage_scaling: String,
-    pub guard: String,
-    pub cancel: String,
-    pub hitconfirm_window: String,
-    pub startup: String,
-    pub active: String,
-    pub recovery: String,
-    pub total: String,
-    pub hitstun: String,
-    pub blockstun: String,
-    pub drive_damage_block: String,
-    pub drive_damage_hit: String,
-    pub drive_gain: String,
-    pub super_gain_hit: String,
-    pub super_gain_block: String,
-    pub projectile_speed: String,
-    pub invuln: String,
-    pub armor: String,
-    pub airborne: String,
-    pub juggle_start: String,
-    pub juggle_increase: String,
-    pub juggle_limit: String,
-    pub perfect_parry_advantage: String,
-    pub after_dr_hit: String,
-    pub after_dr_block: String,
-    pub dr_cancel_hit: String,
-    pub dr_cancel_block: String,
-    pub punish_advantage: String,
-    pub hit_advantage: String,
-    pub block_advantage: String,
-    pub notes: String
-}
 
 pub struct LazyLock<T, F = fn() -> T> {
     data: std::sync::OnceLock<T>,
